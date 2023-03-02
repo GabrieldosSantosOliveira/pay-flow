5eba783492beb3e181191d9c0b95cff3a46410e3
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-02 11:13:33 -0400

    feat: add component Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import { TextInput, TextInputProps } from "react-native";
+import { forwardRef, ForwardRefRenderFunction, memo } from "react";
+import { useInput } from "../hooks/useInput";
+const InputBase: ForwardRefRenderFunction<TextInput, TextInputProps> = (
+  { style, ...props },
+  ref
+) => {
+  const { isFocus, setIsFocus } = useInput();
+  return (
+    <TextInput
+      style={[{ flex: 1, height: "100%" }, style]}
+      ref={ref}
+      {...props}
+      onFocus={() => setIsFocus(true)}
+      onBlur={() => setIsFocus(false)}
+    />
+  );
+};
+export const Input = memo(forwardRef(InputBase));

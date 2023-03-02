9817933ddc64c294ba9f63321bbb8cdc04f3e112
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-02 12:59:38 -0400

    feat: add path alias

--- App.tsx
+++ App.tsx
@@ -1,32 +1,5 @@
 import React from "react";
-import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
-import { Input } from "./src/components/Input";
-import { InputRoot } from "./src/components/InputRoot";
-import { FontAwesome } from "@expo/vector-icons";
+import { HomePage } from "@screens/HomePage";
 export default function App() {
-  return (
-    <KeyboardAvoidingView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <InputRoot>
-          <FontAwesome name="lock" />
-          <Input />
-        </InputRoot>
-        <InputRoot>
-          <FontAwesome name="lock" />
-          <Input />
-        </InputRoot>
-      </View>
-    </KeyboardAvoidingView>
-  );
+  return <HomePage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    paddingHorizontal: 20,
-    gap: 10,
-  },
-});
---
+++ src/screens/HomePage.tsx
@@ -0,0 +1,4 @@
+import { View } from "react-native";
+export const HomePage = () => {
+  return <View style={{ flex: 1 }}></View>;
+};

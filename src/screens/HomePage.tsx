56d6b5fd0ad4f09a752b27b0d2f5f7e4b2560ca7
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-03 13:33:54 -0400

    feat: add screen HomePage

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,4 +1,65 @@
-import { View } from 'react-native';
+import LogoMini from '@assets/logomini.svg';
+import PersonSvg from '@assets/person.svg';
+import { ButtonLoginGoogle } from '@components/Button/ButtonLoginGoogle';
+import { useAuth } from '@hooks/useAuth';
+import { StatusBar } from 'expo-status-bar';
+import { View, useWindowDimensions, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 export const HomePage = () => {
-  return <View style={{ flex: 1 }}></View>;
+  const insets = useSafeAreaInsets();
+  const { height } = useWindowDimensions();
+  const { SingInWithGoogle } = useAuth();
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        alignItems: 'center',
+      }}
+    >
+      <StatusBar backgroundColor="#BF6415" />
+      <View
+        style={{
+          backgroundColor: '#FF941A',
+          width: '100%',
+          height: height * 0.36,
+          alignItems: 'center',
+        }}
+      >
+        <PersonSvg
+          style={{ marginTop: height * 0.08 }}
+          width={208}
+          height={300}
+        />
+      </View>
+      <View
+        style={{
+          marginTop: height * 0.08,
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+          flexDirection: 'column',
+          paddingHorizontal: 40,
+        }}
+      >
+        <LogoMini />
+        <Text
+          style={{
+            textAlign: 'center',
+            color: '#585666',
+            lineHeight: 40,
+            fontSize: 32,
+            fontFamily: 'Lexend_600SemiBold',
+            paddingHorizontal: 30,
+          }}
+        >
+          Organize seus boletos em um só lugar
+        </Text>
+        <ButtonLoginGoogle onPress={SingInWithGoogle} />
+      </View>
+    </View>
+  );
 };

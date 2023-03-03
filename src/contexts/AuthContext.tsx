879dbaa5983a15d2eb4d68313260467e34148ac2
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-03 15:11:55 -0400

    feat: loaded the authenticated user when loading

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,25 +1,37 @@
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { createContext, ReactNode, FC, useEffect, useState } from 'react';
 import { Platform } from 'react-native';
+
 interface IAuthContext {
   SingInWithGoogle: () => Promise<void>;
+  user: FirebaseAuthTypes.User | null;
+  isLoadingUser: boolean;
+  isAuthenticated: boolean;
+  isLoadingAuth: boolean;
 }
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 interface IAuthProvider {
   children: ReactNode;
 }
 export const AuthProvider: FC<IAuthProvider> = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const [isLoadingUser, setIsLoadingUser] = useState<boolean>(false);
+  const [isLoadingAuth, setIsLoadingAuth] = useState(true);
   const SingInWithGoogle = async () => {
     try {
-      await GoogleSignin.revokeAccess();
+      setIsLoadingUser(true);
       await GoogleSignin.hasPlayServices({
         showPlayServicesUpdateDialog: true,
       });
-      const userInfo = await GoogleSignin.signIn();
-      setUser(userInfo);
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      const { user } = await auth().signInWithCredential(googleCredential);
+      setUser(user);
     } catch (error) {
-      console.log(JSON.stringify(error));
+      console.log(error);
+    } finally {
+      setIsLoadingUser(false);
     }
   };
   const configGoogleSingIn = () => {
@@ -38,9 +50,21 @@ export const AuthProvider: FC<IAuthProvider> = ({ children }) => {
   };
   useEffect(() => {
     configGoogleSingIn();
+    auth().onAuthStateChanged((userState) => {
+      setUser(userState);
+      setIsLoadingAuth(false);
+    });
   }, []);
   return (
-    <AuthContext.Provider value={{ SingInWithGoogle }}>
+    <AuthContext.Provider
+      value={{
+        SingInWithGoogle,
+        isLoadingUser,
+        user,
+        isAuthenticated: Boolean(user),
+        isLoadingAuth,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );

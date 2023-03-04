d0c1eb9d64b70dadfc58b280427ed7a45f59cc04
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-03 20:57:04 -0400

    add loading for  font Inter_500Medium

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import 'react-native-gesture-handler';
-
 import { SplashScreen } from '@components/SplashScreen';
 import { AuthProvider } from '@contexts/AuthContext';
-import { Inter_600SemiBold, Inter_400Regular } from '@expo-google-fonts/inter';
+import {
+  Inter_600SemiBold,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
 import {
   Lexend_600SemiBold,
   Lexend_400Regular,
   useFonts,
 } from '@expo-google-fonts/lexend';
 import { Routes } from '@routes/index';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 export default function App() {
   const [isFontsLoaded] = useFonts({
@@ -16,10 +20,13 @@ export default function App() {
     Inter_600SemiBold,
     Lexend_400Regular,
     Inter_400Regular,
+    Inter_500Medium,
   });
   return (
     <AuthProvider>
       <SafeAreaProvider>
+        <StatusBar backgroundColor="#BF6415" />
+
         {isFontsLoaded ? <Routes /> : <SplashScreen />}
       </SafeAreaProvider>
     </AuthProvider>

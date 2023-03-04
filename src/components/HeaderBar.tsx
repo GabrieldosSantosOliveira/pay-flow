09ab55b8182e13d1fa2fc602652463e1e644d24c
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-03 20:58:25 -0400

    feat: add component HeaderBar

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { PixelRatio } from 'react-native';
+import { View } from 'react-native';
+
+import { BoletoInfo } from './BoletoInfo';
+interface Props {
+  countOfBoletos: number;
+}
+export const HeaderBar: FC<Props> = ({ countOfBoletos }) => {
+  return (
+    <View
+      style={{
+        height: PixelRatio.getPixelSizeForLayoutSize(20),
+        width: '100%',
+        backgroundColor: '#FF941A',
+        paddingHorizontal: 30,
+        marginBottom:
+          PixelRatio.getPixelSizeForLayoutSize(40) -
+          PixelRatio.getPixelSizeForLayoutSize(15),
+      }}
+    >
+      <BoletoInfo countOfBoletos={countOfBoletos} />
+    </View>
+  );
+};

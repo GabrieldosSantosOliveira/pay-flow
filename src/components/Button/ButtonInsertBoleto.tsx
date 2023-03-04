3b48580cc3893be6115e0b844865526415d1ac09
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-04 14:25:33 -0400

    feat: add component ButtonInsertBoleto

---
+++ src/components/Button/ButtonInsertBoleto.tsx
@@ -0,0 +1,30 @@
+import { Touchable } from '@components/Touchable';
+import { ResponsiveFontScale } from '@utils/ResponsiveFontScale';
+import { FC } from 'react';
+import { Text, TouchableOpacityProps, View } from 'react-native';
+
+export const ButtonInsertBoleto: FC<TouchableOpacityProps> = ({ ...props }) => {
+  return (
+    <Touchable {...props}>
+      <View
+        style={{
+          backgroundColor: 'white',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 50,
+          width: '100%',
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: 'Inter_400Regular',
+            color: '#585666',
+            fontSize: ResponsiveFontScale(14),
+          }}
+        >
+          Inserir código do boleto
+        </Text>
+      </View>
+    </Touchable>
+  );
+};

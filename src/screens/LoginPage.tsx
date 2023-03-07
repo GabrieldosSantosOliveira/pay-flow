99291c6e1b5d842c2e40582ab086f7aabc97a5dc
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-07 13:39:51 -0400

    feat: set orientation for PORTRAIT

--- src/screens/LoginPage.tsx
+++ src/screens/LoginPage.tsx
@@ -2,12 +2,14 @@ import LogoMini from '@assets/logomini.svg';
 import PersonSvg from '@assets/person.svg';
 import { ButtonLoginGoogle } from '@components/Button/ButtonLoginGoogle';
 import { useAuth } from '@hooks/useAuth';
-import { View, useWindowDimensions, Text } from 'react-native';
+import { useChangeOrientation } from '@hooks/useChangeOrientation';
+import { View, useWindowDimensions, Text, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 export const LoginPage = () => {
   const insets = useSafeAreaInsets();
   const { height } = useWindowDimensions();
   const { SingInWithGoogle, isLoadingUser } = useAuth();
+  useChangeOrientation();
   return (
     <View
       style={{
@@ -44,18 +46,7 @@ export const LoginPage = () => {
         }}
       >
         <LogoMini />
-        <Text
-          style={{
-            textAlign: 'center',
-            color: '#585666',
-            lineHeight: 40,
-            fontSize: 32,
-            fontFamily: 'Lexend_600SemiBold',
-            paddingHorizontal: 30,
-          }}
-        >
-          Organize seus boletos em um só lugar
-        </Text>
+        <Text style={styles.text}>Organize seus boletos em um só lugar</Text>
         <ButtonLoginGoogle
           isLoading={isLoadingUser}
           onPress={SingInWithGoogle}
@@ -65,3 +56,13 @@ export const LoginPage = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    color: '#585666',
+    lineHeight: 40,
+    fontSize: 32,
+    fontFamily: 'Lexend_600SemiBold',
+    paddingHorizontal: 30,
+  },
+});

865750d349dbe73bb7f3897d955cb3f526fcf7c9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-07 13:37:31 -0400

    fix: change navigator stack for bottom-tab

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,14 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { LoginPage } from '@screens/LoginPage';
-const { Navigator, Screen } = createStackNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 export const AppRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="LoginPage" component={LoginPage} />
+      <Screen
+        name="LoginPage"
+        component={LoginPage}
+        options={{ tabBarStyle: { height: 0, opacity: 0 } }}
+      />
     </Navigator>
   );
 };
